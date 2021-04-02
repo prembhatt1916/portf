78bf8a05482cb529457eb0e7c6b6e274df1ccd0e
prembhatt1916 <64638416+prembhatt1916@users.noreply.github.com>
2021-04-02 18:35:19 +0530

    Add files via upload

---
+++ home/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from . models import Portfolio_Home , Review , Contact
+# Register your models here.
+
+admin.site.register(Portfolio_Home)
+admin.site.register(Review)
+admin.site.register(Contact)
